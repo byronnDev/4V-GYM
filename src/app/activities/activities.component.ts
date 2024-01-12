d6f3511dad98b20cde0c59c9960168a1b52104ec
MikelEcheve123 <126151278+MikelEcheve123@users.noreply.github.com>
2024-01-12 01:36:48 +0100

    Refactor activities and header components

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-activities',
   standalone: true,
-  imports: [],
-  templateUrl: './activities.component.html',
-  styleUrl: './activities.component.scss'
+  imports: [MatCardModule, MatDatepickerModule, MatNativeDateModule],
+  template: `
+  <div class="flex justify-around top-0">
+      <mat-card class="demo-inline-calendar-card">
+        <mat-calendar [(selected)]="selected"></mat-calendar>
+      </mat-card>
+      <p>Selected date: {{selected}}</p>
+  </div>
+  `,
+  styles: `
+  .demo-inline-calendar-card {
+    width: 300px;
+  }
+  `
 })
 export class ActivitiesComponent {
+  selected: Date | null;
 
+  constructor() {
+    this.selected = null;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,8 +5,17 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   selector: 'app-header',
   standalone: true,
   imports: [MatToolbarModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  template: `
+  <header class="w-full h-32 bg-neutral-300">
+    <img class="float-start w-28 h-28 m-2 ml-14" src="../../assets/mancuerna.png" alt="4VGYM">
+    <div class="flex justify-center text-custom-red">
+        <h2 class="text-center font-bold text-9xl" style="transform: scale(3); margin-top: 3%; margin-left: -14%;">4V
+            GYM
+        </h2>
+    </div>
+</header>
+`,
+  styles: ``
 })
 export class HeaderComponent {
 
