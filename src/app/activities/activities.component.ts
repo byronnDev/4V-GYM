5270325c4339a049a5fb6ea87d7c12fb2408ece7
MikelEcheve123 <126151278+MikelEcheve123@users.noreply.github.com>
2024-01-12 15:19:08 +0100

    Refactor activities component layout and add date navigation

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -8,31 +8,41 @@ import { GlobalApiService, Activity, ActivityType, Monitors } from '../global-ap
   selector: 'app-activities',
   template: `
       <!-- Calendar -->
-      <div class="absolute top-0 left-0">
+      <div class="fixed top-1/4 left-16">
         <mat-card class="demo-inline-calendar-card">
           <mat-calendar [(selected)]="selected"></mat-calendar>
         </mat-card>
-        <p>Selected date: {{selected}}</p>
       </div>
-
+      
       <!-- List dates -->
-      <div class="flex justify-center items-center h-screen">
-        <table class="bg-neutral-400">
+      <div class="flex text-3xl flex-col justify-center items-center h-screen">
+        <div class="flex flex-row mb-10">
+            <button (click)="restarDia()">
+            <img class="h-16" src="../../assets/flechas.png" alt="flecha">
+            </button>
+          <p class="text-4xl">{{selected.toLocaleDateString('es', { year: 'numeric', month: 'long', day: 'numeric' })}}</p>
+          <button (click)="sumarDia()">
+            <img class="h-16 rotate-180" src="../../assets/flechas.png" alt="flecha">
+          </button>
+        </div>
+        <table class="bg-neutral-300">
           <tbody>
+            @for (activity of activites; track $index) {
             <tr>
                 <td class="bg-white">
-                  <p>{{hours[0]}}</p>
+                  <p>{{hours[$index]}}</p>
                 </td>
                 <td>Monitores</td>
                 <td>Actividad</td>
               </tr>
+            }
           </tbody>
         </table>
       </div>
   `,
 })
 export class ActivitiesComponent {
-  selected: Date | null = null;
+  selected: Date = new Date();
   hours = ['10:00 11:30', '13:30 15:00', '17:30 19:00'];
   activites: Activity[] = [];
   activityTypes: ActivityType[] = [];
@@ -42,6 +52,13 @@ export class ActivitiesComponent {
     this.activites = this.globalApi.activities;
     this.activityTypes = this.globalApi.activityTypes;
     this.monitors = this.globalApi.monitors;
+  }
+
+  sumarDia() {
+    this.selected.setDate(this.selected.getDate() + 1);
+  }
+  restarDia() {
+    this.selected.setDate(this.selected.getDate() - 1);
   }
 }
 
