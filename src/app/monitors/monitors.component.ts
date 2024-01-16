4e434d3bb1eb1f6f0c1ee3b6a1c7b37eabde0f96
MikelEcheve123 <126151278+MikelEcheve123@users.noreply.github.com>
2024-01-16 21:03:05 +0100

    Show 3 cards, arrows and browser

--- src/app/monitors/monitors.component.ts
+++ src/app/monitors/monitors.component.ts
@@ -5,8 +5,9 @@ import { CommonModule } from '@angular/common';
 
 interface Card {
   id: number;
-  title: string;
-  description: string;
+  name: string;
+  email: string;
+  phone: string;
 }
 
 @Component({
@@ -14,64 +15,57 @@ interface Card {
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div class="relative">
-      <div class="flex space-x-6">
-        <button (click)="prevCard()" class="absolute top-0 left-0 z-10 p-4 bg-blue-500 text-white rounded-full
-    focus:outline-none">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-    stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
+    <div class="flex flex-wrap items-center justify-center border-2 border-custom-red rounded-lg w-3/5 bg-neutral-300 mb-20 mx-auto">
+      <img src="../../assets/symbols_search.png" alt="search" class="w-8 h-8 m-2">
+      <input type="text" class="flex-grow p-2 outline-none font-bold text-custom-red bg-neutral-300 text-2xl">
+    </div>
+    
+    <div class="flex flex-wrap space-x-10 justify-center items-center mb-48">
+        <button (click)="scrollLeft()">
+          <img src="../../assets/flechas.png" alt="flecha" class="h-16">
         </button>
-        
-        <div class="w-full">
-          <div class="flex space-x-6">
-            <!-- Aquí se renderizarán las tarjetas -->
-            <div *ngFor="let card of cards" class='bg-gray-100 rounded-lg shadow p-4 w-full'> <!-- Añade las clases y estilos aquí -->
-              <h2 class="text-xl font-semibold mb-2">{{ card.title }}</h2>
-              <p class="whitespace-pre-wrap">{{ card.description }}</p>
+      @for (data of cards; track $index) {
+        @if ($index <= 2) { <!-- Only show 3 cards -->
+          <div class="bg-neutral-300 w-1/4">
+            <div class="flex justify-center">
+              <img src="../../assets/bi_person-fill.png" alt="person" class="h-36 w-36">
+            </div>
+            <div class="text-center">
+              <p>{{data.name}}</p>
+              <a href="mailto:{{data.email}}">{{data.email}}</a>
+              <p>{{data.phone}}</p>
             </div>
           </div>
-        </div>
-
-        <button (click)="nextCard()" class="absolute top-0 right-0 z-10 p-4 bg-blue-500 text-white rounded-full
-    focus:outline-none">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-    stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 19.5L15.75 12l-7.5-7.5" />
-          </svg>
-        </button>
-      </div>
+        }
+      }
+      <button (click)="scrollRight()">
+        <img src="../../assets/flechas.png" alt="flecha" class="h-16 rotate-180">
+      </button>
     </div>
-  `,
+    `,
   styles: `
+
   `
 })
-export class MonitorsComponent implements OnInit {
+export class MonitorsComponent {
   cards: Card[] = [
-    { id: 1, title: 'Card 1', description: 'Este es el contenido de la tarjeta 1.' },
-    { id: 2, title: 'Card 2', description: 'Este es el contenido de la tarjeta 2.' },
-    { id: 3, title: 'Card 3', description: 'Este es el contenido de la tarjeta 3.' },
-    { id: 4, title: 'Card 4', description: 'Este es el contenido de la tarjeta 4.' },
-    { id: 5, title: 'Card 5', description: 'Este es el contenido de la tarjeta 5.' }
+    { id: 0, name: 'Miguel Goyena', email: 'miguel_goyena@cuatrovientos.org', phone: '643231413' },
+    { id: 1, name: 'Ander', email: 'ander@gmail.com', phone: '643551231' },
+    { id: 2, name: 'Jon', email: 'jon@gmail.com', phone: '948231413' },
+    { id: 3, name: 'Iñigo', email: 'inigo_jimenez@cuatrovientos.org', phone: '543231413' },
   ];
-  currentIndex = 0;
 
-  ngOnInit(): void { }
+  firstCardIndex = 0; // índice del primer card que se muestra
 
-  prevCard(): void {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-    } else {
-      this.currentIndex = this.cards.length - 1;
+  scrollLeft() {
+    if (this.firstCardIndex > 0) {
+      this.firstCardIndex--;
     }
   }
 
-  nextCard(): void {
-    if (this.currentIndex < this.cards.length - 1) {
-      this.currentIndex++;
-    } else {
-      this.currentIndex = 0;
+  scrollRight() {
+    if (this.firstCardIndex < this.cards.length - 3) {
+      this.firstCardIndex++;
     }
   }
 
